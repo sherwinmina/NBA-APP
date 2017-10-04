8aca4fcefa0994b2a01aeef6f267c60f19cc6c82
Sherwin Mina <sherwinmina@yahoo.com>
2017-10-03 21:51:13 -0700

    starting server routes

--- client/components/Teams/Index.js
+++ client/components/Teams/Index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Teams extends Component {
+  componentDidMount() {
+    axios.get('http://api.suredbits.com/nba/v0/games')
+      .then( data => console.log(data))
+  }
+
   render () {
     return (
       <div>
