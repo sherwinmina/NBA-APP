ce97bf3cc3f735fa0b219d625a5efc302fc2e538
Sherwin Mina <sherwinmina@yahoo.com>
2017-10-25 23:03:37 -0700

    updating api server

---
+++ server/routes/scoreboard.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var router = express.Router()
+var axios = require('axios')
+
+router.get('/', function(req, res, next) {
+  function todaysGame() {
+    return axios
+      .get('http://api.suredbits.com/nba/v0/games')
+      .then(response => response.data)
+  }
+
+  function getStats() {
+    return axios
+      .get('http://api.suredbits.com/nba/v0/stats/curry/stephen')
+      .then(response => response.data)
+  }
+
+  function getBackground() {
+    return axios
+      .get('http://stats.nba.com/stats/commonplayerinfo?PlayerID=201939')
+      .then(response => response.data)
+  }
+
+  axios.all([todaysGame(), getStats()]).then(
+    axios.spread(function(info, stats) {
+      res.send({ todaysGame, stats })
+    })
+  )
+})
--- server/server.js
+++ server/server.js
@@ -1,72 +1,73 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 var cors = require('cors')
 
 // routes
-var index = require('./routes/index');
-var users = require('./routes/users');
-var games = require('./routes/games');
-var teams = require('./routes/teams');
+var index = require('./routes/index')
+var scoreBoard = require('./routes/scoreboard')
+var users = require('./routes/users')
+var games = require('./routes/games')
+var teams = require('./routes/teams')
 var players = require('./routes/players')
 
-var app = express();
+var app = express()
 
 app.set('port', process.env.PORT || 3001)
 
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(cors());
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(cors())
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/games', games);
-app.use('/players', players);
-app.use('/teams', teams);
+app.use('/', index)
+app.use('/users', users)
+app.use('/scoreboard', scoreBoard)
+app.use('/games', games)
+app.use('/players', players)
+app.use('/teams', teams)
 
-// cors 
+// cors
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   )
-  next();
+  next()
 })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`) // eslint-disable-line no-console
 })
 
-module.exports = app;
+module.exports = app
